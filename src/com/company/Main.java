f312f07c6bf3dda9af30a53ac16554c333e50913
Chelsea Chen <chelseachen893@gmail.com>
2020-08-07 18:27:25 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double kilometre = (100 * 1609344);
+        int highscore = 50;
+        if (highscore == 50){ // in brackets is expression and entire line is a statement
+            System.out.println("this is an expression");
+
+        }
+
+
+	// write your code here
+    }
+}
